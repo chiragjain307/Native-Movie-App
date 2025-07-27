c608b89477c202fa120f5fc105542173c3247dfa
chiragjain307 <chiragjain.jain7@gmail.com>
2025-07-27 21:03:16 +0530

    Updates.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Index: React.FC = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-5xl text-accent font-bold'>Welcome hello new</Text>
+      {/* <Link href='/onboarding'>Onboarding</Link>
+      <Link href='/movie/avangers'>Avanger Movie</Link> */}
+    </View>
+  );
+}
+
+export default Index
